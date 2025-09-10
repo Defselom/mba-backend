a219c9febc96cb1b06cdb9944638a690cd62ba1c
defselom <selomadzomla@gmail.com>
2025-09-10 10:57:45 +0000

    🐳 chore(model): Prisma schema with enums and user models

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
@@ -14,6 +16,17 @@ async function bootstrap() {
     }),
   );
   app.use(cookieParser(process.env.JWT_SECRET));
+
+  const config = new DocumentBuilder()
+    .setTitle('MBA API')
+    .setDescription('The MBA API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 
