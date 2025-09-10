e38b4236505c1f14a6f9469ac828b260d21fba87
defselom <selomadzomla@gmail.com>
2025-09-10 12:36:29 +0000

    ✨ feat(validation): Add role profile validation for user registration

---
+++ src/auth/validation/index.ts
@@ -0,0 +1,1 @@
+export * from './role-profile-register.validator';
---
+++ src/auth/validation/role-profile-register.validator.ts
@@ -0,0 +1,19 @@
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+
+import { RegisterDto } from '@/auth/dto';
+
+@ValidatorConstraint({ name: 'RoleProfileRegister', async: false })
+export class RoleProfileRegisterConstraint implements ValidatorConstraintInterface {
+  validate(value: any, args: ValidationArguments) {
+    const obj = args.object as RegisterDto;
+
+    return !!(obj.participantProfile || obj.speakerProfile || obj.moderatorProfile);
+  }
+  defaultMessage() {
+    return 'Provide at least one profile: participantProfile, speakerProfile, or moderatorProfile.';
+  }
+}
