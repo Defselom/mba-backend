6dce9ed880492d2fb1cbd06e022ee3e97f73d264
defselom <selomadzomla@gmail.com>
2025-09-15 12:45:08 +0000

    ✨ feat(upload): enhance upload functionality with improved error handling and add getUploadFolder utility

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,10 +1,12 @@
 // src/upload/upload.controller.ts
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiConsumes, ApiTags, ApiBody } from '@nestjs/swagger';
+import { ApiConsumes, ApiTags, ApiBody, ApiResponse } from '@nestjs/swagger';
 
 import { UploadFileDto } from './dto/upload-file.dto';
 import { UploadService } from './upload.service';
+import type { MulterFile } from '@/upload/strategies/minio.strategy';
+import { getUploadFolder } from '@/upload/utils';
 
 @ApiTags('upload')
 @Controller('upload')
@@ -14,15 +16,32 @@ export class UploadController {
   @Post()
   @ApiConsumes('multipart/form-data')
   @ApiBody({
-    description: 'Fichier à uploader',
+    description: 'File to upload',
     type: UploadFileDto,
   })
+  @ApiResponse({
+    status: 201,
+    description: 'File uploaded successfully',
+    schema: {
+      example: {
+        message: 'File uploaded successfully!',
+        url: 'https://minio.example.com/bucketname/path/to/file.jpg',
+        key: 'path/to/file.jpg',
+      },
+    },
+  })
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    const uploaded = await this.uploadService.uploadFile(file, 'uploads');
+  async uploadFile(@UploadedFile() file: MulterFile) {
+    if (!file) {
+      throw new Error('File is required');
+    }
+
+    const folder = getUploadFolder(file);
+
+    const uploaded = await this.uploadService.uploadFile(file, folder);
 
     return {
-      message: 'Fichier uploadé !',
+      message: 'File uploaded successfully!',
       ...uploaded,
     };
   }
