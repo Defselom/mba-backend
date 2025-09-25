63bc6d6b8e6e1ca91c097b63094cc963738d57f6
defselom <selomadzomla@gmail.com>
2025-09-25 10:16:46 +0000

    🐞 fix(utils): enhance generateBaseUrl function to remove pagination parameters

--- src/shared/utils/get-base-url.utils.ts
+++ src/shared/utils/get-base-url.utils.ts
@@ -1,5 +1,10 @@
 import { Request } from 'express';
 
-export function generateBaseUrl(request: Request) {
-  return `${request.protocol}://${request.get('host')}${request.path}`;
+export function generateBaseUrl(request: Request): string {
+  const url = new URL(`${request.protocol}://${request.get('host')}${request.originalUrl}`);
+
+  url.searchParams.delete('page');
+  url.searchParams.delete('limit');
+
+  return `${url.origin}${url.pathname}${url.search}`;
 }
